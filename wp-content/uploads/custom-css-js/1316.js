5d4837e76a2bab01f410e1068818498e45494f2e
matheusollima <matheus.ollima98@gmail.com>
2025-11-15 14:15:42 -0300

    update

---
+++ wp-content/uploads/custom-css-js/1316.js
@@ -0,0 +1,6 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+let teste = document.querySelector(".menu-item-1317");
+teste.style.color = "red";
+</script>
+<!-- end Simple Custom CSS and JS -->
---
+++ wp-content/uploads/custom-css-js/1323.js
@@ -0,0 +1,12 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+document.addEventListener('DOMContentLoaded', () => {
+	const botao = document.querySelector('.baixar-curriculo');
+	const link = botao.querySelector('a');
+	link.style.color = "#4DFF2C";
+	link.setAttribute('href', "http://portflio.local/wp-content/uploads/2025/11/Curriculo-Matheus-Oliveira-Lima-Atualizado-Nov-2024.pdf");
+	link.setAttribute("download", "Curriculo-Matheus-Oliveira.pdf");
+
+})
+</script>
+<!-- end Simple Custom CSS and JS -->
